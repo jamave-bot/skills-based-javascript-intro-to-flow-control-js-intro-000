13d8be7b37c271d8316edeeaaa0fbed23b2e3e2e
Jason V <jamave777@gmail.com>
2021-03-30 04:35:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,23 +34,25 @@ function switchAge(age) {
     case 13:
       return "You are a teenager"
       break;
-    case 13:
+    case 14:
       return "You are a teenager"
       break;
-    case 13:
+    case 15:
       return "You are a teenager"
       break;
-    case 13:
+    case 16:
       return "You are a teenager"
       break;
-    case 13:
+    case 17:
       return "You are a teenager"
       break;
-    case 13:
+    case 18:
       return "You are a teenager"
       break;
-    case 13:
+    case 19:
       return "You are a teenager"
       break;
+    default:
+      return "You have an age"  
   }
 }
